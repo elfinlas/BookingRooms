c0415e49195cf25e53c8548b6a8badeae3b74cd1
MHLab <elfinlas>
2018-12-01 23:07:31 +0900

    [18. 12. 01]
    * 자동 로그인 기능 구현
    * CompanyBoard 를 기반으로 기능 붙임
    * resolved #35

--- src/main/java/com/mhlab/br/config/WebMvcConfig.java
+++ src/main/java/com/mhlab/br/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.mhlab.br.config;
 
 import com.mhlab.br.component.interceptors.AuthInterceptor;
+import com.mhlab.br.component.interceptors.AutoLoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private AuthInterceptor authInterceptor;
+    private AutoLoginInterceptor autoLoginInterceptor;
 
-    public WebMvcConfig(AuthInterceptor authInterceptor) {
+    public WebMvcConfig(AuthInterceptor authInterceptor, AutoLoginInterceptor autoLoginInterceptor) {
         this.authInterceptor = authInterceptor;
+        this.autoLoginInterceptor = autoLoginInterceptor;
     }
 
     @Override
@@ -24,6 +27,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/users/login", "/users/login/*", "/users/logout", "/users/signup")
                 .excludePathPatterns("/js/**", "/css/**", "/lib/**"); //로그인 쪽은 예외처리를 한다.
+
+        registry.addInterceptor(autoLoginInterceptor)
+                .addPathPatterns("/users/login");
     }
 
 }
